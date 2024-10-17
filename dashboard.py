af6d3effb5ea57da58a484f4c5c25fb38f0b6cdc
landyking <landykingdom@gmail.com>
2024-10-17 19:02:32 +0800

    download all stacks

--- dashboard.py
+++ dashboard.py
@@ -8,7 +8,8 @@ import duckdb
 import json
 from datetime import datetime,timezone,timedelta
 
-selected_auto_refresh = st.sidebar.checkbox(label="Auto Refresh",value=True)
+selected_auto_refresh = st.sidebar.checkbox(label="Auto Refresh",value=True)    
+
 @st.fragment(run_every="10s" if selected_auto_refresh else None)
 def main_fragment():
     result_stacks = api.get_stacks()
@@ -125,27 +126,13 @@ def main_fragment():
         select_row = all_stacks.iloc[select_row_idx]
         st.session_state['search_stack_id'] = select_row.get('id')
         st.switch_page("stacks.py")
-    # stack_list = all_stacks.to_dict(orient="records")
-    # while stack_list:
-    #     cols = st.columns(3)
-    #     for i in range(3):
-    #         if not stack_list:
-    #             break
-    #         stack = stack_list.pop(0)
-    #         with cols[i].container(border=True):
-    #             # st.json(stack,expanded=1)
-    #             state = stack['state']
-    #             components = stack.get('components',[])
-    #             if type(components) is not list:
-    #                 components = []
-    #             color = "green" if state == 'StackConsistency' else 'red'
-    #             st.markdown(f"##### :{color}[{stack['id']}]")
-    #             st.markdown(f" **State**: :{color}[{stack['state']}]")
-    #             st.markdown(f" **Last Modified**: `{stack['last_modified_str']}`")
-    #             comps_md =" ".join([f"`{it['name']}`" for it in components])
-    #             st.markdown(f" **Components**: {comps_md}")
-    #             if st.button(label="View",type="secondary",use_container_width=True,key=f"comp_btn_{stack['id']}"):
-    #                 st.session_state['search_stack_id'] = stack['id']
-    #                 st.switch_page("stacks.py")
+    
+    st.divider()
+    
+    download_data = json.dumps(result_stacks,indent=4)
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    st.download_button(label="Download All Stacks",
+                    data=download_data,
+                    file_name=f"all_stacks.{timestamp}.json",mime="text/json",type="secondary")
 
 main_fragment()
